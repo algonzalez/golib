ce91a7b45ef607ad9ac07590db01b701a31c548d
Al Gonzalez <al.gonzo@outlook.com>
2022-05-06 23:38:14 -0400

    Update fs package
    - Add GetSearchPaths
    - Move IsCaseSensitive to os pkg

--- fs/fs.go
+++ fs/fs.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 	"io/fs"
 	"os"
-	"strings"
+	"path/filepath"
+
+	"github.com/algonzalez/golib/path"
+	"github.com/algonzalez/golib/slice"
 )
 
 // DirExists returns true if the directory path was found
@@ -21,20 +24,6 @@ func FileExists(path string) bool {
 	return !errors.Is(err, fs.ErrNotExist) && !info.IsDir()
 }
 
-// GetFileInfo returns an fs.FileInfo representing the file at path.
-// If the file was not found, it will return a custom error.
-// If there is another error, it will be of type *fs.PathError
-func GetFileInfo(path string) (fs.FileInfo, error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		return nil, err
-	}
-	if info.IsDir() {
-		return nil, errors.New("requested path is a directory not file")
-	}
-	return info, err
-}
-
 // GetDirInfo returns an fs.FileInfo representing the directory at path.
 // If the file was not found, it will return a custom error.
 // If there is another error, it will be of type *fs.PathError
@@ -49,21 +38,32 @@ func GetDirInfo(path string) (fs.FileInfo, error) {
 	return info, err
 }
 
-// IsCaseSensitive determines if the file system is case sensitive or not.
-func IsCaseSensitive() (bool, error) {
-	file, err := os.CreateTemp(os.TempDir(), "gonzal-golib-fs-IsCaseSensitive-*.TMP")
+// GetFileInfo returns an fs.FileInfo representing the file at path.
+// If the file was not found, it will return a custom error.
+// If there is another error, it will be of type *fs.PathError
+func GetFileInfo(path string) (fs.FileInfo, error) {
+	info, err := os.Stat(path)
 	if err != nil {
-		return false, err
+		return nil, err
 	}
-	closeError := file.Close()
-	defer func() {
-		if closeError == nil {
-			os.Remove(file.Name())
-		}
-	}()
+	if info.IsDir() {
+		return nil, errors.New("requested path is a directory not file")
+	}
+	return info, err
+}
 
-	if _, err := os.Stat(strings.ToLower(file.Name())); errors.Is(err, os.ErrNotExist) {
-		return true, closeError
+// GetSearchPaths returns a distinct list of paths
+// defined in the PATH environment value.
+// Also includes the current working directory in the list.
+func GetSearchPaths() []string {
+	if pathString, ok := os.LookupEnv("PATH"); ok {
+		paths := filepath.SplitList(pathString)
+
+		// include current directory
+		cwd, _ := os.Getwd()
+		paths = slice.Prepend(paths, cwd)
+
+		return slice.ToDistinct(paths, path.NormalizePath)
 	}
-	return false, closeError
+	return []string{}
 }
